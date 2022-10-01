701aa9f874e4cd60989b4e18c4de8c61a9d0d145
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-30 23:48:47 -0400

    style: ajustando detalhes header logo + tittle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,26 @@
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+
 import "./Header.css";
 
 import logo from "../../assets/logo.png";
 
 export function Header() {
     return (
-        <header>
-            <img src={logo} alt="logo do aplicativo" />
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
+        <header className="site__header">
+            <figure className="logo__tittle--container">
+                <img src={logo}
+                    alt="app logo"
+                    className="logo"
+                />
+                <h1 className="site__tittle">Delivery App</h1>
+            </figure>
+            <nav className="nav__menu">
+                <Link to={"/"}>Home</Link>
+                <Link to={"menu"}>Menu</Link>
+                <Link to={"about"}>About Us</Link>
+                <Link to={"service"}>Service</Link>
+                <FaShoppingCart />
             </nav>
         </header>
     )
