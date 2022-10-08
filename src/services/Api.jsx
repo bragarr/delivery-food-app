1d302859fc3414429292de9fdbca13d232e353a9
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-10-08 01:35:41 -0400

    fix: API Link paraSeção The best na HomePage

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -2,7 +2,7 @@ const API_CATEGORY = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
 const API_FILTER = "https://www.themealdb.com/api/json/v1/1/filter.php?c="
 
-const API_RAMDOM = "https:/www.themealdb.com/api/json/v1/1/random.php"
+const API_RAMDOM = "https://www.themealdb.com/api/json/v1/1/random.php"
 
 
 export { API_CATEGORY, API_FILTER, API_RAMDOM };
